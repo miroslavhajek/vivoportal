931edf5f10c860ba025917429afc85b9a484f2b0
David Lukas <david.lukas@zfdaily.com>
2013-08-02 18:04:28 +0200

    Select element enables disabling of the InArray validator

--- src/Vivo/Form/Element/Select.php
+++ src/Vivo/Form/Element/Select.php
@@ -4,9 +4,28 @@ namespace Vivo\Form\Element;
 use Zend\Form\Element\Select as ZendSelect;
 use Zend\Form\ElementPrepareAwareInterface;
 use Zend\Form\FormInterface;
+use Zend\Validator\Explode as ExplodeValidator;
+use Zend\Validator\InArray as InArrayValidator;
+use Zend\Validator\StringLength as StringLengthValidator;
 
 class Select extends ZendSelect implements ElementPrepareAwareInterface
 {
+    /**
+     * Should the default InArray validator be disabled?
+     * ZF2 2.2 provides this feature out of the box
+     * @var bool
+     */
+    protected $disableInArrayValidator  = false;
+
+    /**
+     * Disable the default InArray validator
+     * @param bool $disable
+     */
+    public function setDisableInArrayValidator($disable)
+    {
+        $this->disableInArrayValidator  = (bool) $disable;
+    }
+
     /**
      * Prepare the form element (mostly used for rendering purposes)
      * @param FormInterface $form
@@ -20,5 +39,22 @@ class Select extends ZendSelect implements ElementPrepareAwareInterface
             $id = str_replace('[', '-', $id);
             $this->setAttribute('id', $id);
         }
+    }
+
+    /**
+     * Overridden getValidator() to disable the default InArray validator
+     * @return \Zend\Validator\ValidatorInterface
+     */
+    protected function getValidator()
+    {
+        if (null === $this->validator) {
+            if (!$this->disableInArrayValidator) {
+                parent::getValidator();
+            } else {
+                    $validator = new StringLengthValidator(array('min' => 0));
+                    $this->validator = $validator;
+            }
+        }
+        return $this->validator;
     }
 }
