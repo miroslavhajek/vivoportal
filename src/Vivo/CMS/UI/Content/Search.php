870490d170819fbd0bb711501fde994dcb3ca5fa
Miroslav Hajek <mirek@fearless.cz>
2013-09-06 13:59:44 +0200

    Implementation

--- src/Vivo/CMS/UI/Content/Search.php
+++ src/Vivo/CMS/UI/Content/Search.php
@@ -3,6 +3,7 @@ namespace Vivo\CMS\UI\Content;
 
 use Vivo\CMS\Api\CMS;
 use Vivo\CMS\Api\Document as DocumentApi;
+use Vivo\CMS\Model;
 use Vivo\CMS\UI\AbstractForm;
 use Vivo\CMS\Model\Content\Search as SearchModel;
 use Vivo\UI\Paginator;
@@ -23,7 +24,7 @@ class Search extends AbstractForm
    /**
     * @var int
     */
-   public $max_score = 1;
+   private $maxScore = 1;
 
     /**
      * @var \Vivo\CMS\Api\CMS
@@ -39,7 +40,7 @@ class Search extends AbstractForm
      * Document API
      * @var DocumentApi
      */
-    protected $documentApi;
+    private $documentApi;
 
     /**
      * @var SiteEvent
@@ -50,19 +51,19 @@ class Search extends AbstractForm
      * Paginator
      * @var $paginator
      */
-    protected $paginator;
+    private $paginator;
 
     /**
      * Search result
      * @var array
      */
-    protected $result;
+    private $result;
 
     /**
      * Searched result count
      * @var int
      */
-    protected $numRows;
+    private $numRows;
 
     /**
      * Constructor
@@ -83,14 +84,14 @@ class Search extends AbstractForm
         $this->documentApi = $documentApi;
         $this->siteEvent   = $siteEvent;
         $this->paginator   = $paginator;
+        $this->autoAddCsrf = false;
     }
 
     public function attachListeners()
     {
         parent::attachListeners();
 
-        $this->getEventManager()->attach(ComponentEventInterface::EVENT_INIT,
-            array($this, 'initListenerSearchInit'));
+        $this->getEventManager()->attach(ComponentEventInterface::EVENT_INIT, array($this, 'initListenerSearchInit'));
     }
 
     public function initListenerSearchInit()
@@ -101,7 +102,7 @@ class Search extends AbstractForm
 
         $this->paginator->setItemsPerPage($this->content->getPageSize());
 
-        $this->view->max_score = $this->max_score;
+        $this->view->maxScore = $this->maxScore;
         $this->view->paginator = $this->paginator;
 
         //Add params to paginator
@@ -130,117 +131,119 @@ class Search extends AbstractForm
      * TODO: move to Vivo\CMS\Api\Content\Search
      * @param array $data
      */
-    public function doSearch(array $data)
+    protected function doSearch(array $data)
     {
-        $qb = new QueryBuilder();
-        $documentList = array();
+        $wordsString = trim($data['words']);
+        if ($wordsString != '') {
+            $qb = new QueryBuilder();
+            $documentList = array();
 
-        if($data['operator']) {
-            $operator = $data['operator'];
-        } else {
-            $operator = $this->content->getOperator();
-        }
-        if($data['wildcards']) {
-            $wildcards = $data['wildcards'];
-        } else {
-            $wildcards = $this->content->getWildcards();
-        }
-
-        $words  = $this->addFieldConstraintToQueryParts(explode(' ', $data['words']));
-
-        $query = '';
-        $currentQuery = '';
-        foreach ($words as $key => $word) {
-            if($operator != 'exact_op') {
-                if($wildcards == 'start_asterisk') {
-                    $word = '*' . $word;
-                } elseif($wildcards == 'end_asterisk') {
-                    $word = $word . '*';
-                } elseif($wildcards == 'start_end_asterisk') {
-                    $word = '*' . $word . '*';
-                }
-            }
-            $query = $qb->cond($word);
-            if($currentQuery instanceof QueryInterface){
-                if($operator == 'exact_op') {
-                    $currentQuery = $qb->cond(join(' ', $words));
-                    break;
-                } elseif($operator == 'and_op') {
-                    $currentQuery = $qb->andX($currentQuery, $query);
-                } elseif($operator == 'or_op') {
-                    $currentQuery = $qb->orX($currentQuery, $query);
-                }
+            if($data['operator']) {
+                $operator = $data['operator'];
             } else {
-                $currentQuery = $query;
+                $operator = $this->content->getOperator();
             }
-        }
-
-        $currentQuery = $qb->andX($currentQuery, $qb->cond($this->siteEvent->getSite()->getPath() . '/*','\path'));
-
-        $params = array();
-
-        // Limit and offset of paginator are not used because results are filtered next and count does not match.
-        $params['page_size'] = SearchModel::$SEARCH_LIMIT;
-        $result = $this->indexer->find($currentQuery, $params);
-
-        $this->numRows = $result->getTotalHitCount();
-
-        foreach ($result->getHits() as $hit) {
-            $entityPath = $hit->getDocument()->getField('\path')->getValue();
-            try {
-                $entity = $this->cmsApi->getSiteEntity($entityPath, $this->siteEvent->getSite());
-            } catch (EntityNotFoundException $e) {
-                $this->numRows -= 1;
-                continue;
+            if($data['wildcards']) {
+                $wildcards = $data['wildcards'];
+            } else {
+                $wildcards = $this->content->getWildcards();
             }
-            if ($entity instanceof \Vivo\CMS\Model\Document) {
-                if (!$this->documentApi->isPublished($entity)) {
-                    $this->numRows -= 1;
+
+            $words  = $this->addFieldConstraintToQueryParts(explode(' ', $wordsString));
+            $query = null;
+            $currentQuery = null;
+            foreach ($words as $key => $word) {
+                if($operator != 'exact_op') {
+                    if($wildcards == 'start_asterisk') {
+                        $word = '*' . $word;
+                    } elseif($wildcards == 'end_asterisk') {
+                        $word = $word . '*';
+                    } elseif($wildcards == 'start_end_asterisk') {
+                        $word = '*' . $word . '*';
+                    }
+                }
+                $query = $qb->cond($word);
+                if($currentQuery instanceof QueryInterface){
+                    if($operator == 'exact_op') {
+                        $currentQuery = $qb->cond($wordsString);
+                        break;
+                    } elseif($operator == 'and_op') {
+                        $currentQuery = $qb->andX($currentQuery, $query);
+                    } elseif($operator == 'or_op') {
+                        $currentQuery = $qb->orX($currentQuery, $query);
+                    }
+                } else {
+                    $currentQuery = $query;
+                }
+            }
+
+            $currentQuery = $qb->andX($currentQuery,
+                                      $qb->cond($this->siteEvent->getSite()->getPath() . '/ROOT/*', '\path'));
+
+            $params = array();
+            // Limit and offset of paginator are not used because results are filtered next and count does not match.
+            $params['page_size'] = SearchModel::$SEARCH_LIMIT;
+            $result = $this->indexer->find($currentQuery, $params);
+
+            $this->numRows = $result->getTotalHitCount();
+
+            foreach ($result->getHits() as $hit) {
+                $entityPath = $hit->getDocument()->getField('\path')->getValue();
+                try {
+                    $entity = $this->cmsApi->getSiteEntity($entityPath, $this->siteEvent->getSite());
+                } catch (EntityNotFoundException $e) {
+                    $this->numRows--;
                     continue;
                 }
-            } elseif ($entity instanceof \Vivo\CMS\Model\Content) {
-                $entity = $this->cmsApi->getParent($this->cmsApi->getParent($entity));
-                $entityPath = $entity->getPath();
-                if (!$this->documentApi->isPublished($entity)) {
-                    $this->numRows -= 1;
-                    continue;
+
+                if ($entity instanceof Model\Content) {
+                    $document = $this->documentApi->getContentDocument($entity);
+                    if ($document instanceof Model\Document) {
+                        $entity = $document;
+                        $entityPath = $document->getPath();
+                    }
+                    else {
+                        $this->numRows--;
+                        continue;
+                    }
+                }
+
+                if ($entity instanceof Model\Document
+                && !isset($documentList[$entityPath])
+                && $entity->getSearchable()
+                && $this->documentApi->isPublished($entity)) {
+                    $documentList[$entityPath]['document'] = $entity;
+                    $documentList[$entityPath]['score'] = $hit->getScore();
+                } else {
+                    $this->numRows--;
                 }
             }
-            if ($entity instanceof \Vivo\CMS\Model\Document && $entity->getSearchable() && !isset($documentList[$entityPath])) {
-                $documentList[$entityPath]['title'] = $entity->getTitle();
-                $documentList[$entityPath]['description'] = $entity->getDescription();
-                $documentList[$entityPath]['url'] = $this->cmsApi->getEntityRelPath($entity);
-                $documentList[$entityPath]['score'] = $hit->getScore();
-            } else {
-                $this->numRows -= 1;
-            }
-        }
 
-        $this->paginator->setItemCount($this->numRows);
-        //Choose pages from result
-        $resultPages = array_chunk($documentList, $this->content->getPageSize());
-        $this->result = $resultPages[$this->paginator->getPage() - 1];
-        $this->view->result = $this->result;
+            $this->paginator->setItemCount($this->numRows);
+            //Choose pages from result
+            $resultPages = array_chunk($documentList, $this->content->getPageSize());
+            $this->result = $resultPages[$this->paginator->getPage() - 1];
+            $this->view->result = $this->result;
+        }
     }
 
     /**
-     * @param array $query_parts
+     * @param array $queryParts
      * @return array
      */
-    protected function addFieldConstraintToQueryParts(array $query_parts)
+    protected function addFieldConstraintToQueryParts(array $queryParts)
     {
-        $new_array = array();
-        foreach ($query_parts as $part) {
-            $new_array[] = mb_strtolower($part, mb_detect_encoding($part));
+        $array = array();
+        foreach ($queryParts as $part) {
+            $array[] = mb_strtolower($part, mb_detect_encoding($part));
         }
-        return $new_array;
+        return $array;
     }
 
     protected function doGetForm()
     {
         $form = new Form('Searchform');
         $form->setAttribute('method', 'get');
-
         $form->add(array(
             'name' => 'act',
             'attributes' => array(
@@ -278,7 +281,7 @@ class Search extends AbstractForm
             'name' => 'submit',
             'type' => 'Vivo\Form\Element\Submit',
             'attributes' => array(
-                'value' => 'Search',
+                'value' => 'Search', //FIXME: Neodesilat slovo Search
             ),
         ));
 
