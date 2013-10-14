69df639a03269e4906cf232cffd4159e7284de04
David Lukas <david.lukas@vivoportal.org>
2013-10-14 20:22:39 +0200

    Session cookie fix

--- src/Vivo/Service/SessionManagerFactory.php
+++ src/Vivo/Service/SessionManagerFactory.php
@@ -27,17 +27,18 @@ class SessionManagerFactory implements FactoryInterface
         //Configure cookie according to the security
         if (isset($config['session_config']['security']['enabled'])
                 && $config['session_config']['security']['enabled']) {
-            if (isset($config['session_config']['security']['secure_ports'])
-                && in_array($_SERVER['SERVER_PORT'], $config['session_config']['security']['secure_ports'])) {
-                //Access via secure port
-                $sessionConfigOptions['cookie_httponly']    = false;
-                $sessionConfigOptions['cookie_secure']      = true;
-                $sessionConfigOptions['name']
-                    = $config['session_config']['security']['secure_session_name'];
-            } else {
-                //Access via 'normal' port
-                $sessionConfigOptions['cookie_httponly']    = true;
-                $sessionConfigOptions['cookie_secure']      = false;
+            $request    = $serviceLocator->get('request');
+            if ($request instanceof \Zend\Http\PhpEnvironment\Request) {
+                $headerName = $config['session_config']['security']['security_http_header'];
+                if ($request->getHeader($headerName) || $_SERVER['HTTPS']) {
+                    $sessionConfigOptions['cookie_httponly']    = false;
+                    $sessionConfigOptions['cookie_secure']      = true;
+                    $sessionConfigOptions['name']
+                        = $config['session_config']['security']['secure_session_name'];
+                } else {
+                    $sessionConfigOptions['cookie_httponly']    = true;
+                    $sessionConfigOptions['cookie_secure']      = false;
+                }
             }
         }
         $sessionConfigObj   = new \Zend\Session\Config\SessionConfig();
